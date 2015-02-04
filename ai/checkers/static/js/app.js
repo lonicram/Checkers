354fd221173ffa0e7dbb8277c4f53f55e0a2eb8c
marcin-olejniczak <marcin@seamlessmedical.com>
2015-02-04 21:43:53 +0100

    Small changes in board layout

--- ai/checkers/static/js/app.js
+++ ai/checkers/static/js/app.js
@@ -1,1 +1,32 @@
- 
+var checkers = (function(){
+    "use strict";
+    var self = {};
+    self.turn = '';
+    
+    var make_move = function(){
+        
+    };
+    var show_av_moves = function(pawn){
+        self.turn = ($(this).hasClass('white')) ? 'white' : 'black';
+        pawn.addClass('active');
+        var row = pawn.parents('tr').index();
+        var col = pawn.index();
+        
+    };
+    var gui = function(){
+        var pawns = $('#board .pawn');
+        pawns.on('click tap', function(){
+            pawns.removeClass('active');
+            show_av_moves($(this));
+            
+        });
+    };
+    self.init = function(){
+        gui();
+    };
+    return self;
+})();
+
+$(document).ready(function(){
+    checkers.init();
+});
